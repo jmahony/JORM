fa1fa86c3348cecdaa4ba5677ef716a9ce0a9b91
Josh Mahony <jalmahony@gmail.com>
2014-10-18 09:44:15 +0100

    Make persistentcontext extend from expandable

--- PersistentContext.java
+++ PersistentContext.java
@@ -1,14 +1,9 @@
 package com.wagerwilly.jorm;
 
 import java.lang.reflect.Field;
-import java.util.Map;
 
-public class PersistentContext {
-    public Class c;
+public class PersistentContext extends ExpandablePersistentContext {
     public String tableName;
-    public String[] columns;
-    public Field[] fields;
-    public Map<Class, ExpandablePersistentContext> expandablePersistents;
     public String insertQuery;
     public String selectQuery;
     public String updateQuery;
