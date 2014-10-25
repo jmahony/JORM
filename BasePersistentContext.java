b825f6efaf59f66c22d809b58c8e860bf132a092
Josh Mahony <jalmahony@gmail.com>
2014-10-25 11:14:56 +0100

    Remove expandable persistent list from base persistent context

--- BasePersistentContext.java
+++ BasePersistentContext.java
@@ -2,11 +2,9 @@ package com.wagerwilly.jorm;
 
 import java.lang.reflect.Field;
 import java.util.List;
-import java.util.Map;
 
 public class BasePersistentContext {
     public Class c;
-    public Map<Class, BasePersistentContext> expandablePersistents;
     public Field containingField;
     public List<PersistentUnit> persistentUnits;
 }
