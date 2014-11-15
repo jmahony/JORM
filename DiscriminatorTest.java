4b877d6f63a446ca452958ba08264b62dda98072
Josh Mahony <jalmahony@gmail.com>
2014-11-15 15:16:49 +0000

    Finished discriminator

--- DiscriminatorTest.java
+++ DiscriminatorTest.java
@@ -1,55 +1,54 @@
 package com.wagerwilly.jorm;
 
-import com.wagerwilly.jorm.annotations.Persistent;
-import com.wagerwilly.jorm.annotations.Table;
+import com.wagerwilly.jorm.annotations.Discriminator;
+import com.wagerwilly.jorm.exceptions.DiscriminatorException;
 import org.junit.Test;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class DiscriminatorTest {
-
-    @Table(name = "users")
-    @Discriminator(name = UserDiscriminator.class)
-    private class User {
-        @Persistent
-        String name;
-    }
-
-    private class Administrator extends User {}
-
-    @interface Discriminator {
-        Class name();
-    }
-
-    interface DiscriminatorInterface {
-        public Class execute(Object o) throws Exception;
-    }
-
-    private class UserDiscriminator implements DiscriminatorInterface {
-        @Override
-        public Class execute(Object o) throws Exception {
-            ResultSet rs = (ResultSet) o;
-            String discriminatorValue = rs.getString("role");
-            switch (discriminatorValue) {
-                case "administrator":
-                    return Administrator.class;
-                case "user":
-                    return User.class;
-            }
-            throw new Exception("Invalid discriminator value");
-        }
-    }
-
     @Test
-    public void testSimpleDiscriminator() throws SQLException {
+    public void testSimpleDiscriminator() throws Exception {
         ResultSet rs = mock(ResultSet.class);
         when(rs.getString("role")).thenReturn("administrator");
-        Class o = Administrator.class;
-        assertEquals(Administrator.class.getCanonicalName(), o.getCanonicalName());
+
+        Class o = DiscriminatorEvaluator.discriminate(rs, User.class);
+
+        assertEquals(Administrator.class, o);
+    }
+
+    @Test(expected = DiscriminatorException.class)
+    public void testClassNoDiscriminatorAnnotation() throws Exception {
+        ResultSet rs = mock(ResultSet.class);
+        when(rs.getString("role")).thenReturn("administrator");
+
+        DiscriminatorEvaluator.discriminate(rs, UserWithNoDiscriminator.class);
     }
 }
+
+@Discriminator(discriminatorClass = UserDiscriminator.class)
+class User {}
+
+class Administrator extends User {}
+
+class UserDiscriminator implements DiscriminatorInterface {
+    public UserDiscriminator() {}
+    @Override
+    public Class execute(Object o) throws Exception {
+        ResultSet rs = (ResultSet) o;
+        String discriminatorValue = rs.getString("role");
+        switch (discriminatorValue) {
+            case "administrator":
+                return Administrator.class;
+            case "user":
+                return User.class;
+        }
+        throw new Exception("Invalid discriminator value");
+    }
+}
+
+class UserWithNoDiscriminator {}
